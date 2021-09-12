4404d9f93b7af850e5da238e3cc5119bf41c5555
Arseniy Parshukov <catkitkat.ars@yandex.ru>
2021-09-12 18:36:37 +0500

    Решил задачу 2.5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  maxlength = str.length;
+    if (maxlength > 20) {
+        return str.slice(0, 19) + '...';
+    } else {
+        return str;
+    }
 }
