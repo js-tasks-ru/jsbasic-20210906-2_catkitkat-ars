0aa538dcd3ee02f0f714755210c7606a4040acfe
Arseniy Parshukov <catkitkat.ars@yandex.ru>
2021-09-13 11:40:30 +0500

    Исправил

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,11 +1,6 @@
 function checkSpam(str) {
     if (!str) return str;
     let spam = str.toUpperCase()
-    if (spam.includes('1XBET')){
-        return true;
-    } else if (spam.includes('XXX')) {
-        return true;
-    } else {
-        return false;
-    }
+    return spam.includes('1XBET') || spam.includes('XXX');
 }
+
