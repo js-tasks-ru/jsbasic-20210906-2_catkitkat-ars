1d6419c02f78a09735cefa2c666d31acd1103169
Arseniy Parshukov <catkitkat.ars@yandex.ru>
2021-09-14 16:42:30 +0500

    Решил задачи 3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  metka:
+  for (let key in salaries){
+    if (typeof salaries[key] == 'number') {
+      if (isFinite(salaries[key])) {
+        sum += salaries[key];
+      } else {
+        continue metka;
+      } 
+    } 
+  } return sum;
 }
