c6e10fcfbff480e23dbedab493a10369ed96d535
Arseniy Parshukov <catkitkat.ars@yandex.ru>
2021-09-15 10:19:26 +0500

    Исправил

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,13 +1,10 @@
 function sumSalary(salaries) {
   let sum = 0;
-  metka:
   for (let key in salaries){
-    if (typeof salaries[key] == 'number') {
-      if (isFinite(salaries[key])) {
-        sum += salaries[key];
-      } else {
-        continue metka;
-      } 
-    } 
+    if (typeof salaries[key] == 'number' && isFinite(salaries[key])) {
+      sum += salaries[key];
+    } else {
+      continue;
+    }
   } return sum;
 }
